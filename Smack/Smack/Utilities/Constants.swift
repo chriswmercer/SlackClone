e0d2f1c058fc29a5d8187a3d52f5ded09f9c18a2
Unknown <chriswm@me.com>
2020-04-15 15:06:16 +0100

    Lesson 10 - Web Request

--- Smack/Smack/Utilities/Constants.swift
+++ Smack/Smack/Utilities/Constants.swift
@@ -8,6 +8,19 @@
 
 import Foundation
 
+//api url
+let BASE_URL = "https://chriswm-chatter.herokuapp.com/v1/"
+let URL_REGISTER = BASE_URL + "account/register"
+
+//completion handler
+typealias  CompletionHandler = (_ Success: Bool) -> ()
+
+//user defaults
+let LOGGED_IN_KEY = "loggedIn"
+let TOKEN_KEY = "token"
+let USER_EMAIL_KEY = "userEmail"
+
+// segues
 let TO_LOGIN = "segueToLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND_TO_CHANNEL = "unwindToChannel"
