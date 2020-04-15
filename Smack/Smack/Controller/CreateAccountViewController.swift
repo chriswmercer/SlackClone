023da4997260cbc8becb7b32b0fc3bc5eadec637
Unknown <chriswm@me.com>
2020-04-15 15:27:33 +0100

    Lesson 11

--- Smack/Smack/Controller/CreateAccountViewController.swift
+++ Smack/Smack/Controller/CreateAccountViewController.swift
@@ -26,6 +26,14 @@ class CreateAccountViewController: UIViewController {
     }
     
     @IBAction func createAccountButtonPressed(_ sender: Any) {
+        guard let username = usernameTextField.text, usernameTextField.text != "" else { return }
+        guard let email = emailTextField.text , emailTextField.text != "" else { return }
+        guard let password = passwordTextField.text, passwordTextField.text != "" else { return }
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            if success {
+                print("registered user")
+            }
+        }
     }
     
     @IBAction func chooseAvatarButtonPressed(_ sender: Any) {
