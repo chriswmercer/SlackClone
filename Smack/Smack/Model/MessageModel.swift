b208454ec57278155165f1c6febcef4f734c942d
Unknown <chriswm@me.com>
2020-04-18 16:24:30 +0100

    Lesson 25 and 26 - messages

---
+++ Smack/Smack/Model/MessageModel.swift
@@ -0,0 +1,9 @@
+//
+//  MessageModel.swift
+//  Smack
+//
+//  Created by Chris Mercer on 18/04/2020.
+//  Copyright © 2020 Chris Mercer. All rights reserved.
+//
+
+import Foundation
---
+++ Smack/Smack/Support/KeyboardBoundView.swift
@@ -0,0 +1,21 @@
+//
+//  KeyboardBoundView.swift
+//  Smack
+//
+//  Created by Chris Mercer on 18/04/2020.
+//  Copyright © 2020 Chris Mercer. All rights reserved.
+//
+
+import UIKit
+
+class KeyboardBoundView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
